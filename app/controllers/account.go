54d8fc87ee51a2d719de4d10c051777c4c08df21
busyStone <jlu.xzp@gmail.com>
2013-12-24 23:44:33 +0800

    1 Optimize the e-mail address regex 2 CN->En

--- app/controllers/account.go
+++ app/controllers/account.go
@@ -14,39 +14,36 @@ var USERNAME_REX, PWD_REX, NICKNAME_REX *regexp.Regexp
 
 func init() {
 
-	USERNAME_REX = regexp.MustCompile(`^[0-9a-zA-Z]+@(([0-9a-zA-Z]+)[.])+[a-z]{2,4}$`)
+	USERNAME_REX = regexp.MustCompile(`^([\w\.\_]{2,10})@(\w{1,}).([a-z]{2,4})$`)
 	PWD_REX = regexp.MustCompile(`^[\x01-\xfe]{8,20}$`)
 
 	NICKNAME_REX = regexp.MustCompile(`^[a-z0-9_]{6,16}$`)
 }
 
 ///////////////////////////////////////////////////////////////////////////
-// 注册
+// Register
 func (c *Account) Register() revel.Result {
-	return c.Render()
-}
-
-func (c *Account) RegisterSuccessful() revel.Result {
+	c.Flash.Error("")
 	return c.Render()
 }
 
 func (c *Account) PostRegister(user *models.MockUser) revel.Result {
 
-	c.Validation.Required(user.Email).Message("邮箱地址未填写！")
-	c.Validation.Check(user.Email, revel.Match{USERNAME_REX}).Message("邮箱地址格式错误！")
+	c.Validation.Required(user.Email).Message("Email not filled!")
+	c.Validation.Check(user.Email, revel.Match{USERNAME_REX}).Message("Email format error!")
 
-	c.Validation.Required(user.Nickname).Message("昵称未填写！")
-	c.Validation.Check(user.Nickname, revel.Match{NICKNAME_REX}).Message("昵称格式错误！")
+	c.Validation.Required(user.Nickname).Message("Nickname not filled!")
+	c.Validation.Check(user.Nickname, revel.Match{NICKNAME_REX}).Message("Nickname format error!")
 
-	c.Validation.Required(user.Password).Message("密码未填写！")
-	c.Validation.Check(user.Password, revel.Match{PWD_REX}).Message("密码格式错误！")
+	c.Validation.Required(user.Password).Message("Password is not filled!")
+	c.Validation.Check(user.Password, revel.Match{PWD_REX}).Message("Password format error!")
 
-	c.Validation.Required(user.ConfirmPassword == user.Password).Message("两次输入密码不一致！")
+	c.Validation.Required(user.ConfirmPassword == user.Password).Message("Please confirm the password!")
 
 	if c.Validation.HasErrors() {
 		c.Validation.Keep()
 		c.FlashParams()
-		return c.Redirect((*Account).Register) // 用 c.Register 会提示错误
+		return c.Redirect((*Account).Register)
 	}
 
 	err := user.Register()
@@ -58,49 +55,40 @@ func (c *Account) PostRegister(user *models.MockUser) revel.Result {
 		return c.Redirect((*Account).Register)
 	}
 
-	// 在客户端 的存储是 签名明文
-	// 直接这样写会有问题
-	// 应该咋写捏？？
-	c.Session["email"] = user.Email
-	c.Session["password"] = user.Password
-
-	c.Flash.Success("注册并登录成功！")
+	c.Flash.Success("Register & Login successfully!")
 
 	return c.Redirect((*Home).Index)
 }
 
 ///////////////////////////////////////////////////////////////////////////
-// 登录
+// Login
 func (c *Account) Login() revel.Result {
 	return c.Render()
 }
 
 func (c *Account) PostLogin(loginuser *models.LoginUser) revel.Result {
 
-	c.Validation.Required(loginuser.Email).Message("邮箱地址未填写！")
-	c.Validation.Check(loginuser.Email, revel.Match{USERNAME_REX}).Message("邮箱地址格式错误！")
+	c.Validation.Required(loginuser.Email).Message("Email not filled!")
+	c.Validation.Check(loginuser.Email, revel.Match{USERNAME_REX}).Message("Email format error!")
 
-	c.Validation.Required(loginuser.Password).Message("密码未填写！")
-	c.Validation.Check(loginuser.Password, revel.Match{PWD_REX}).Message("密码格式错误！")
+	c.Validation.Required(loginuser.Password).Message("Password is not filled!")
+	c.Validation.Check(loginuser.Password, revel.Match{PWD_REX}).Message("Password format error!")
 
 	ok, _ := loginuser.IsRegistered()
 	if !ok {
-		c.Flash.Error("Email或密码错误！")
+		c.Flash.Error("Email or Password is incorrect!")
 
 		c.FlashParams()
 		return c.Redirect((*Account).Login)
 	}
 
-	c.Session["email"] = loginuser.Email
-	c.Session["password"] = loginuser.Password
-
-	c.Flash.Success("登录成功！")
+	c.Flash.Success("Login successfully!")
 
 	return c.Redirect((*Home).Index)
 }
 
 ///////////////////////////////////////////////////////////////////////////
-// 退出
+// Logout
 func (c *Account) Logout() revel.Result {
 
 	for k := range c.Session {
